a07702101d9415b935a42068c28f53e398e23453
nmtoan <nmtoan.cmu@gmail.com>
2016-06-25 22:06:08 +0700

    Creating the Product Repository

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
